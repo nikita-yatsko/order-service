12f8ec69c35a064175d032cf9a34e1c4a248c0eb
nikit <nikitatayatsko308@gmail.com>
2025-11-24 22:08:04 +0300

    db_schema

---
+++ src/main/java/com/order/service/order_service/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.order.service.order_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
