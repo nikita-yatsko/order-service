3e3e955d20f721d20d9e692b8ded305c98881aee
nikit <nikitatayatsko308@gmail.com>
2025-11-29 16:09:11 +0300

    controller layer and exception handling

--- src/main/java/com/order/service/order_service/service/Impl/OrderServiceImpl.java
+++ src/main/java/com/order/service/order_service/service/Impl/OrderServiceImpl.java
@@ -1,12 +1,17 @@
 package com.order.service.order_service.service.Impl;
 
 import com.order.service.order_service.mapper.OrderMapper;
-import com.order.service.order_service.model.dto.OrderDto;
+import com.order.service.order_service.mapper.OrderResponseMapper;
+import com.order.service.order_service.model.constants.ErrorMessage;
+import com.order.service.order_service.model.dto.UserInfo;
 import com.order.service.order_service.model.entity.Order;
-import com.order.service.order_service.model.enums.Status;
+import com.order.service.order_service.model.entity.OrderItem;
+import com.order.service.order_service.model.exception.NotFoundException;
 import com.order.service.order_service.model.request.OrderRequest;
+import com.order.service.order_service.model.response.OrderResponse;
 import com.order.service.order_service.repository.OrderRepository;
 import com.order.service.order_service.service.OrderService;
+import com.order.service.order_service.service.UserCacheService;
 import com.order.service.order_service.utils.specifications.OrderSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -24,58 +29,70 @@ public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
+    private final OrderResponseMapper orderResponseMapper;
+    private final UserCacheService userCacheService;
+
+    private final static String EMAIL = "admin@example.com"; //TODO idk how to implement it (where will we get email?)
 
     @Override
     @Transactional
-    public OrderDto createOrder(OrderRequest orderRequest) {
+    public OrderResponse createOrder(OrderRequest orderRequest) {
         Double totalPrice = orderRequest.getItems()
                 .stream()
-                .mapToDouble(item -> item.getItem().getPrice())
+                .mapToDouble(item -> item.getItem().getPrice() * item.getQuantity())
                 .sum();
         orderRequest.setTotalPrice(totalPrice);
 
-        Order newOrder = orderRepository.save(orderMapper.createOrder(orderRequest));
-        return orderMapper.toOrderDto(newOrder);
+        Order order = orderMapper.createOrder(orderRequest);
+        for (OrderItem item : order.getOrderItems())
+            item.setOrder(order);
+
+        Order newOrder = orderRepository.save(order);
+        UserInfo userInfo = userCacheService.getUserInfo(EMAIL);
+        return orderResponseMapper.toOrderResponse(userInfo, orderMapper.toOrderDto(newOrder));
     }
 
     @Override
-    public OrderDto getOrderById(Integer id) {
+    public OrderResponse getOrderById(Integer id) {
         return orderRepository.findOrderById(id)
-                .map(orderMapper::toOrderDto)
-                .orElseThrow(() -> new RuntimeException("Order with id " + id + " not found"));
+                .map(order -> orderResponseMapper.toOrderResponse(userCacheService.getUserInfo(EMAIL), orderMapper.toOrderDto(order)))
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.ORDER_NOT_FOUND_BY_ID.getMessage(id)));
     }
 
     @Override
-    public Page<OrderDto> getAllOrders(LocalDateTime from, LocalDateTime to, Status status, Pageable pageable) {
-        Specification<Order> specification = OrderSpecification.dateBetween(from, to);
+    public Page<OrderResponse> getAllOrders(LocalDateTime from, LocalDateTime to, String status, Pageable pageable) {
+        Specification<Order> specification = Specification
+                .anyOf(OrderSpecification.dateBetween(from, to))
+                .and(OrderSpecification.filterByStatus(status));
+
         Page<Order> orders = orderRepository.findAll(specification, pageable);
 
-        return orders.map(orderMapper::toOrderDto);
+        return orders.map(order -> orderResponseMapper.toOrderResponse(userCacheService.getUserInfo(EMAIL), orderMapper.toOrderDto(order)));
     }
 
     @Override
-    public List<OrderDto> getOrdersByUserId(Integer userId) {
+    public List<OrderResponse> getOrdersByUserId(Integer userId) {
         return orderRepository.findAllOrdersByUserId(userId)
                 .stream()
-                .map(orderMapper::toOrderDto)
+                .map(order -> orderResponseMapper.toOrderResponse(userCacheService.getUserInfo(EMAIL), orderMapper.toOrderDto(order)))
                 .toList();
     }
 
     @Override
     @Transactional
-    public OrderDto updateOrderById(Integer id, OrderRequest orderRequest) {
+    public OrderResponse updateOrderById(Integer id, OrderRequest orderRequest) {
         Order order = orderRepository.findOrderById(id)
-                .orElseThrow(() -> new RuntimeException("Order with id " + id + " not found"));
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.ORDER_NOT_FOUND_BY_ID.getMessage(id)));
         orderMapper.updateOrder(orderRequest, order);
 
-        return orderMapper.toOrderDto(orderRepository.save(order));
+        return orderResponseMapper.toOrderResponse(userCacheService.getUserInfo(EMAIL), orderMapper.toOrderDto(order));
     }
 
     @Override
     @Transactional
     public void deleteOrderById(Integer id) {
         if (orderRepository.findOrderById(id).isEmpty())
-            throw new RuntimeException("Order not found");
+            throw new NotFoundException(ErrorMessage.ORDER_NOT_FOUND_BY_ID.getMessage(id));
 
         orderRepository.softDelete(id);
     }
